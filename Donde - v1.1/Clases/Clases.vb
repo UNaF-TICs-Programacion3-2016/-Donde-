342e495d983ba10cfd5b4900219dd1d2271ef851
DARIO-PC\DARIO <jdario_cba@hotmail.com>
2016-11-12 22:48:39 -0300

    Actualizacion Clase ConexionBD y metodos de conecion y carga de tabla

--- Donde - v1.1/Clases/Clases.vb
+++ Donde - v1.1/Clases/Clases.vb
@@ -1,5 +1,72 @@
 ﻿Imports Oracle.DataAccess.Client ' Sentencia para importar la Clase OracleDataAcces
 
+Public Enum TipoAccion As Byte 'Enumeracion para selecionar el tipo de accion a realizar por el usuario
+    Alta = 1
+    Modificacion = 2
+    Baja = 3
+End Enum
+
+
+
+
+Public Class ConexionBD
+
+    Friend Accion As TipoAccion
+    'Friend IdPersona As Long
+
+    'Esquema Desconectado
+
+    'Oracle.DataAcces.Client
+    Public Conexion As OracleConnection
+    Dim AdaptadorOA As OracleDataAdapter
+    'Oracle.System.Data
+    Dim RegistroDS As New DataSet
+    Dim RegistroDR As DataRow
+
+
+    Public Function Conectar() As Boolean
+
+        Dim Estado As Boolean = True
+
+        Try
+            Conexion = New OracleConnection("Data Source=localhost;User Id=Sitio;Password=123456789;")
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Estado = False
+        End Try
+
+        Return Estado
+
+    End Function
+
+    Public Function Cargar_Tabla(Entidad_p As String, Tabla_p As String, Id_tabla_p As String) As Boolean
+
+        Dim Estado As Boolean = True
+
+        Try
+            'Carga en Memoria Principal de la Tabla Persona_Cab
+            AdaptadorOA = New OracleDataAdapter("Select * From " + Entidad_p, Conexion)
+            AdaptadorOA.Fill(RegistroDS, Tabla_p)
+            If Accion = TipoAccion.Alta Then
+                RegistroDR = RegistroDS.Tables(Tabla_p).NewRow()
+                RegistroDR(Id_tabla_p) = -1
+            Else
+                RegistroDR = RegistroDS.Tables(Tabla_p).Rows.Item(0)
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Estado = False
+        End Try
+
+        Return Estado
+
+    End Function
+
+
+End Class
+
 
 
 
--- Donde - v1.1/Donde.vb
+++ Donde - v1.1/Donde.vb
@@ -2,15 +2,29 @@
 
 
 Public Class F_Donde
+    'Atributos
 
-   
+    Dim OconexionDB As New ConexionBD
 
+    'Metodos (unciones y Procedimientos)
+
+    Private Sub F_Donde_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        If OconexionDB.Conectar Then
+            MsgBox("Conectado")
+            OconexionDB.Cargar_Tabla("Sitio_cab", "sitio_cab", "ID_SITIO")
+            MsgBox("Tabla Cargada con Exito")
+        Else
+            MsgBox("No Conectado")
+        End If
+
+    End Sub
 
     Private Sub BtnDSalir_Click(sender As Object, e As EventArgs)
         Me.Close()
     End Sub
 
-    
+
     Private Sub AgregarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AgregarToolStripMenuItem.Click
         F_CargaSitio.Show()
         Me.Hide()
@@ -20,7 +34,7 @@ Public Class F_Donde
         Me.Close()
     End Sub
 
-    
+
     Private Sub RubroToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RubroToolStripMenuItem.Click
         F_CargaRubros.Show()
         F_CargaRubros.GBSubRubro.Visible = False
@@ -32,4 +46,6 @@ Public Class F_Donde
         F_CargaRubros.GBRubro.Visible = False
         Me.Hide()
     End Sub
+
+
 End Class
